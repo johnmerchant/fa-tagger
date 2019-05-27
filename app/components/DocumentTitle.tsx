a3a1939ac026e5506ca1e47e366739bd978eb372
John Merchant <john@merchant.on.net>
2019-05-27 15:15:11 +1000

    Update DocumentTitle.tsx

--- app/components/DocumentTitle.tsx
+++ app/components/DocumentTitle.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 
-
-
 export const DocumentTitle = ({title, children}: { title: string, children?: JSX.Element | JSX.Element[] }) => {
     React.useEffect(() => { 
         document.title = title;
