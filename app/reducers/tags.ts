ce0b2f2a7c1234c5ea75161a819a56eaa6db0a68
John Merchant <john@jmercha.dev>
2019-11-25 06:07:43 +1100

    Fix types

--- app/reducers/tags.ts
+++ app/reducers/tags.ts
@@ -1,15 +1,18 @@
 import produce from 'immer';
 import { Tags } from "../@types/tags";
 import { TagsAction } from '../actions/tags';
+import { Reducer } from 'redux';
 
 export interface State {
     tags?: Tags;
 }
 
-export const tags = produce((state: State, action: TagsAction) => {
+const initialState: State = {};
+
+export const tags: Reducer<State> = produce((state: State, action: TagsAction) => {
     switch (action.type) {
         case 'TAGS_UPDATE':
             state.tags = action.tags;
             break;
     }
-}, {});
+}, initialState);
